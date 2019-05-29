5b9996b11db44f265674b375a23a1cd477a0ae71
routetehpacketz <23489718+routetehpacketz@users.noreply.github.com>
2019-05-28 22:47:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+## Cisco DHCP Reservation Creation
+
+This Python script allows you to easily convert current DHCP leases on Cisco IOS devices to reservations.
+
+Please note that this script is only designed to run on Cisco IOS devices.
+
+### Usage
+
+1. Open a command prompt/terminal and run cisco-dhcp-res.py 
+
+2. Enter the IP of the device hosting the DHCP pool and credentials to SSH to it.
+
+```
+Enter IP of device hosting DHCP: 10.6.1.1
+Username: admin
+Password: ******
+```
+
+The script will then obtain the current leases and let you select one to convert by providing the lease's IP.
+
+```
+IP address       Client-ID/              Lease expiration        Type
+                 Hardware address
+10.1.1.16        0100.1234.ed33.c4       May 29 2019 05:32 PM    Automatic
+10.1.1.17        0100.1234.d620.de       May 28 2019 09:16 PM    Automatic
+10.1.1.18        0100.1234.a67f.80       May 29 2019 04:38 PM    Automatic
+
+Enter the IP of the lease to convert to reservation: 10.1.1.18
+```
+
+The script will also alert you if the provided IP *is a CDP neighbor*. Currently this will not provide you port information past the core router/switch:
+
+```
+Your DHCP reservation has been created:
+
+10.1.1.68        0140.017a.7072.e4       Infinite                Manual
+```
+
+### Requirements
+
+-Python3.x
+
+-Python module 'netmiko'
+
+-SSH access to the Cisco device hosting the DHCP pool
+
---
+++ cisco-dhcp-res.py
@@ -0,0 +1,46 @@
+from netmiko import ConnectHandler
+import time
+import re
+import getpass
+
+ip_regex=re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+clientid_regex=re.compile(r'[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{2}')
+
+dhcp_srv=input("Enter IP of device hosting DHCP: ")
+while not re.match(ip_regex,dhcp_srv):
+	dhcp_srv=input("You must enter an IP address: ")
+	
+username=input("Username: ")
+while not username:
+	username=input("Username: ")
+password=getpass.getpass()
+while not password:
+	password=getpass.getpass()
+
+dhcp_srv_conn=ConnectHandler(device_type='cisco_ios',host=dhcp_srv,username=username,password=password,fast_cli=False)
+leases=dhcp_srv_conn.send_command("show ip dhcp bin")
+print(leases+"\n")
+dict={}
+for line in leases.splitlines():
+	if re.match(ip_regex,line):
+		ip=re.search(ip_regex,line)
+		#ips.append(ip.group())
+		client_id=re.search(clientid_regex,line)
+		x={ip.group():client_id.group()}
+		#client_ids.append(client_id)
+		dict.update(x)
+reservation=input("Enter the IP of the lease to convert to reservation: ")
+while reservation not in dict.keys():
+	reservation=input("Invalid selection. Enter the IP of the lease to convert to reservation: ")
+dhcp_srv_conn.send_command("clear ip dhcp bin "+reservation)
+dhcp_srv_conn.send_config_set("ip dhcp pool "+reservation+"\nhost "+reservation+"\nclient-id "+dict[reservation])
+print("\nYour DHCP reservation has been created:\n")
+print(dhcp_srv_conn.send_command("show ip dhcp bind "+reservation+" | inc "+reservation+"_"))
+
+'''	
+clear ip dhcp binding 10.1.3.85
+conf t
+ip dhcp pool 10.1.3.85
+host 10.1.3.85        
+client-id ?
+'''
