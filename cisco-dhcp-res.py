293ab43312b57c307dd97fa330dc928ec8ca9900
routetehpacketz <23489718+routetehpacketz@users.noreply.github.com>
2019-05-29 23:25:46 -0400

    Added handling for DHCP pool settings
    I realized my first attempt at this script didn't accommodate the full config of the existing DHCP pool. The script will now list the DHCP pools configured on the target device and prompt you to pick which one the target lease should be converted from.

--- cisco-dhcp-res.py
+++ cisco-dhcp-res.py
@@ -18,9 +18,25 @@ while not password:
 	password=getpass.getpass()
 
 dhcp_srv_conn=ConnectHandler(device_type='cisco_ios',host=dhcp_srv,username=username,password=password,fast_cli=False)
+pools=dhcp_srv_conn.send_command("show ip dhcp pool | inc Pool")
+pool_names=[]
+for line in pools.splitlines():
+	line=line.lstrip("Pool ")
+	line=line.rstrip(" :")
+	pool_names.append(line)
+for pool in pool_names:
+	print(pool)
+target_pool=input("Select the DHCP pool for the target lease to convert (pool options will be applied to reservation: ")
+while target_pool not in pool_names:
+	target_pool=input("Select the DHCP pool for the target lease to convert: ")
+pool_config=dhcp_srv_conn.send_command("show run | sec "+target_pool)
+pool_settings=[]
+for line in pool_config.splitlines():
+	if line.startswith("  "):
+		pool_settings.append(line)
 leases=dhcp_srv_conn.send_command("show ip dhcp bin")
 print(leases+"\n")
-dict={}
+lease_dict={}
 for line in leases.splitlines():
 	if re.match(ip_regex,line):
 		ip=re.search(ip_regex,line)
@@ -28,11 +44,13 @@ for line in leases.splitlines():
 		client_id=re.search(clientid_regex,line)
 		x={ip.group():client_id.group()}
 		#client_ids.append(client_id)
-		dict.update(x)
+		lease_dict.update(x)
 reservation=input("Enter the IP of the lease to convert to reservation: ")
-while reservation not in dict.keys():
+while reservation not in lease_dict.keys():
 	reservation=input("Invalid selection. Enter the IP of the lease to convert to reservation: ")
 dhcp_srv_conn.send_command("clear ip dhcp bin "+reservation)
-dhcp_srv_conn.send_config_set("ip dhcp pool "+reservation+"\nhost "+reservation+"\nclient-id "+dict[reservation])
+dhcp_srv_conn.send_config_set("ip dhcp pool "+reservation+"\nhost "+reservation+"\nclient-id "+lease_dict[reservation])
+for x in pool_settings:
+	dhcp_srv_conn.send_config_set("ip dhcp pool "+reservation+"\n"+x)
 print("\nYour DHCP reservation has been created:\n")
 print(dhcp_srv_conn.send_command("show ip dhcp bind "+reservation+" | inc "+reservation+"_"))
